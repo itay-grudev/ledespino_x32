4ce0301c2a3c7fdc784eb684a11e4b7a0c80f827
Itay Grudev <itay+89bf5c@grudev.com>
2019-02-22 23:17:43 +0000

    Making progress towards the new UI

--- main/component.mk
+++ main/component.mk
@@ -2,6 +2,8 @@
 # "main" pseudo-component makefile.
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
-CFLAGS += -pedantic -Wall
-COMPONENT_EMBED_FILES := html/index.html
+# COMPONENT_EXTRA_CLEAN := html/index.html
+# COMPONENT_EMBED_FILES := html/index.html
 # COMPONENT_EMBED_TXTFILES := html/index.html
+
+CFLAGS += -pedantic -Wall
