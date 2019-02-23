87b10494ce351aacc9ac7d3bb5ccedbaefe41200
Itay Grudev <itay+89bf5c@grudev.com>
2019-02-23 22:27:40 +0000

    Python WS testing server

---
+++ backend_stub/websocket_server.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import pathlib
+import asyncio
+import websockets
+
+async def new_socket(websocket, path):
+    setup = open( pathlib.Path(__file__).parent / 'setup', 'r' ).read()
+    await websocket.send( setup )
+
+    while True:
+        await asyncio.sleep( 5 )
+        status = open( pathlib.Path(__file__).parent / 'status', 'r' ).read()
+        await websocket.send( status )
+
+start_server = websockets.serve( new_socket, 'localhost', 8765 )
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
